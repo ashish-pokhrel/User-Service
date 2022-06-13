6c42f7e922dd9178915dea5e51e558da699d70d4
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-06-13 13:08:29 -0500

    Authorization Generic

---
+++ src/main/java/com/user/userservice/controllers/AuthController.java
@@ -0,0 +1,42 @@
+package com.user.userservice.controllers;
+
+import com.user.userservice.models.Admin;
+import com.user.userservice.models.Normal;
+import com.user.userservice.models.User;
+import com.user.userservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/saveAdmin")
+    public String saveAdmin(@RequestBody Admin user) {
+        userService.save(user);
+        return "Saved Successfully";
+    }
+    @PostMapping("/saveNormal")
+    public String saveNormal(@RequestBody Normal user) {
+        userService.save(user);
+        return "Saved Successfully";
+    }
+    @GetMapping("/get/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getById(id);
+    }
+
+    @GetMapping("/getRoleById/{id}")
+    public String getRoleById(@PathVariable Long id) {
+        return userService.getRoleById(id);
+    }
+
+    @GetMapping("/getUserByName/{userName}")
+    public User getUserByName(@PathVariable String userName) {
+        return userService.getUserByUserName(userName);
+    }
+
+}
---
+++ src/main/java/com/user/userservice/repositories/IUserRepo.java
@@ -0,0 +1,15 @@
+package com.user.userservice.repositories;
+
+import com.user.userservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepo extends JpaRepository<User, Long> {
+    @Query("select  u from User u Where u.UserName =:userName")
+    User getUserByUserName(String userName);
+
+    @Query("select u.Role from User u Where u.Id =:id")
+    String getRoleById(long id);
+}
