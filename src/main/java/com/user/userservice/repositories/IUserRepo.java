a9fd9d54295142da7fb4c4758d20a1eff5259294
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-06-13 19:31:59 -0500

    Save Update User

--- src/main/java/com/user/userservice/repositories/IUserRepo.java
+++ src/main/java/com/user/userservice/repositories/IUserRepo.java
@@ -3,12 +3,13 @@ package com.user.userservice.repositories;
 import com.user.userservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserRepo extends JpaRepository<User, Long> {
-    @Query("select  u from User u Where u.Username =:userName")
-    User getUserByUserName(String userName);
+    @Query("select  u from User u Where u.Username =:username")
+    User getUserByUserName(@Param("username") String username);
 
     @Query("select u.Role from User u Where u.Id =:id")
     String getRoleById(long id);
